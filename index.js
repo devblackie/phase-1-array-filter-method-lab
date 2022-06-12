cbc67d6bc7ae835aea1fd4466f77b3d771ad637f
devblackie <newtieblackie@gmail.com>
2022-06-12 18:49:43 +0300

    commit tree 24b8f795098f093893e064535a4f78c21dfd3157

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+const findMatching=(driver, string)=>{
+    const matchingDriver=driver.filter((driver)=>{
+        return driver.toUpperCase()===string.toUpperCase()
+    })
+    return matchingDriver;
+}
+
+const fuzzyMatch=(driver, string)=>{
+    const matching=driver.filter((driver)=>{
+        return driver.slice(0,2)===string.slice(0,2)
+    })
+    return matching;
+}
+
+const matchName=(driver, string)=> {
+    const name= driver.filter((driver)=>{
+        return driver.name===string
+    })
+    return name;
+}
